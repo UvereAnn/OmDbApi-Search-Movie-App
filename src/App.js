e288b93aea9ff7be74d78b5ef038a86a4da994d7
annuvere <40842990+annuvere@users.noreply.github.com>
2021-12-05 22:33:49 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movie, setMovie] = useState({});
+    const [search, setSearch] = useState('');
+    
+    const API_KEY = "745d0d4e";
+    const url = `http://www.omdbapi.com/?t=${search}&apikey=${API_KEY}`;
+    
+    const getMovie = async()=>{
+      try {
+        const response = await fetch(url);
+         const data = await response.json()
+         setMovie(data);
+      } catch (error) {
+          console.error(error);
+      } 
+    }
+
+    const onInputChange = e =>{
+      setSearch(e.target.value);
+    }
+
+    useEffect(()=> {
+      getMovie();
+    }, []);
+
+
+  function checkResponse(data){
+     if(data.Response==="True"){
+       return(
+         <div>
+            <img src={data.Poster} alt=""/>
+            <h4>Title: {data.Title}</h4>
+            <p>Year: {data.Year}</p>
+            <p>Writer: {data.Writer}</p>
+            <p>{data.Response}</p>
+         </div>
+       );
+    }
+      return (
+        <p>No Movie found</p>
+      );
+    }
+
+   return(
+      <div>
+         <input type="text" value={search} onChange={onInputChange}/>
+         <button type="submit" onClick={getMovie}>Search</button>
+         <br></br>   
+         {checkResponse(movie)}
+      </div>
+   );
 }
 
 export default App;
